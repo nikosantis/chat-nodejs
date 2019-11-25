19e341711f793e086cac3a518d69cd04577085a6
Nikolas Santis <nikosantis@gmail.com>
2019-11-24 22:35:09 -0300

    Module Responses

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
+const response = require('./network/response')
+
 const router = express.Router()
 
 var app = express()
@@ -12,13 +15,16 @@ router.get('/message', function (req, res) {
   res.header({
     "custom-header": "Nuestro valor personalizado",
   })
-  res.send('Lista de mensajes')
+  response.succes(req, res, 'Lista de mensajes')
 })
 
 router.post('/message', function (req, res) {
   console.log(req.query)
-  console.log(req.body)
-  res.status(201).send([{error: '', body: 'Creado correctamente'}])
+  if (req.query.error == 'ok') {
+    response.error(req, res, 'Error simulado')
+  } else {
+    response.succes(req, res, 'Creado correctamente', 201)
+  }
 })
 
 // app.use('/', function(req, res) {
