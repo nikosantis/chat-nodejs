36f084b2a549efde425a2214eb6bd0ef1e3a03de
Nikolas Santis <nikosantis@gmail.com>
2019-11-24 21:22:40 -0300

    Router Express.js

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express')
+const router = express.Router()
 
 var app = express()
 
-app.use('/', function(req, res) {
-  res.send('Hola')
+app.use(router)
+
+router.get('/', function (req, res) {
+  res.send('Hola desde /get')
 })
+
+router.post('/', function (req, res) {
+  res.send('Hola desde /post')
+})
+
+router.patch('/', function (req, res) {
+  res.send('Hola desde /patch')
+})
+
+router.delete('/', function (req, res) {
+  res.send('Hola desde /delete')
+})
+
+// app.use('/', function(req, res) {
+//   res.send('Hola')
+// })
 
 app.listen(3000)
 console.log('La aplicación está escuchando en http://localhost:3000')
