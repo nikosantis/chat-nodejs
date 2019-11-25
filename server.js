67b78dd56d647903de676b18efbab8d31c1c2a34
Nikolas Santis <nikosantis@gmail.com>
2019-11-24 22:38:56 -0300

    Statics

--- server.js
+++ server.js
@@ -27,9 +27,7 @@ router.post('/message', function (req, res) {
   }
 })
 
-// app.use('/', function(req, res) {
-//   res.send('Hola')
-// })
+app.use('/app', express.static('public'))
 
 app.listen(3000)
 console.log('La aplicación está escuchando en http://localhost:3000')
